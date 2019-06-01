29c70049d6c1eef18c555c3aac66fa36c44f8ce8
Allan Barbosa <allan.hb@outlook.com>
2019-06-01 15:59:51 -0300

    ajustes para finalizar o profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -8,9 +8,11 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   var phoneMask = new MaskedTextController(mask: '(00) 00000-0000');
+  static final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
@@ -18,67 +20,98 @@ class _ProfilePageState extends State<ProfilePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Nome'
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Name',
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter your name';
+                    }
+                  },
                 ),
-              ),
-              
-              Container(height: 20.0),
-              
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'E-mail'
+                
+                Container(height: 20.0),
+                
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Email'
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Enter your email';
+                    }
+                  },
                 ),
-              ),
 
-              Container(height: 20.0),
-              
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Linkedin',
-                  helperText: '@douglita'
+                Container(height: 20.0),
+                
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Linkedin',
+                    helperText: '@douglita'
+                  ),
                 ),
-              ),
 
-              Container(height: 20.0),
-              
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Celular',
+                Container(height: 20.0),
+                
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Celular',
+                  ),
+                  controller: phoneMask,
+                  keyboardType: TextInputType.number,
                 ),
-                controller: phoneMask,
-                keyboardType: TextInputType.number,
-              ),
-
-              // Container(height: 20.0),
-
-              // Container(
-              //   width: double.maxFinite,
-              //   height: 60,
-              //   child: RaisedButton.icon(
-              //     icon: Icon(Icons.save),
-              //     label: Text('Salvar'),
-              //     onPressed: () {},
-              //   ),
-              // ),
-              
-
-            ],
+              ],
+            ),
           ),
         ), 
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Increment',
         child: Icon(Icons.save),
-        onPressed: () {},
+        onPressed: () {
+          if (_formKey.currentState.validate()) {
+            _showDialog();
+            // Scaffold
+            //   .of(context)
+            //   .showSnackBar(SnackBar(content: Text('Processing Data')));
+          } else {
+          }
+        },
       ),
+    );
+  }
+
+  void _showDialog() {
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Success"),
+          content: new Text("Enjoy the community!"),
+          actions: <Widget>[
+            // usually buttons at the bottom of the dialog
+            new FlatButton(
+              child: new Text("Ok"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
