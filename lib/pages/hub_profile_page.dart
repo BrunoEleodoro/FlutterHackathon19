6b3a38ee508a597af50ce5578f14d17cbfb20378
Bruno Eleodoro Roza <tesopehak@quick-mail.online>
2019-06-01 15:35:54 -0300

    Update hub_profile_page.dart

--- lib/pages/hub_profile_page.dart
+++ lib/pages/hub_profile_page.dart
@@ -27,9 +27,37 @@ class HubProfilePageState extends State<HubProfilePage> {
         Image.network(hub.photoUrl),
         Text(hub.name),
         Text("Teste"),
-
+        Container(
+          height: 400,
+            child: UsersComponent())
       ],)
     );
   }
 
+}
+
+
+class UsersComponent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.event), text: 'Events',),
+              Tab(icon: Icon(Icons.people), text: 'Users',),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.directions_car),
+            Icon(Icons.directions_transit),
+          ],
+        ),
+      ),
+    );
+  }
 }
